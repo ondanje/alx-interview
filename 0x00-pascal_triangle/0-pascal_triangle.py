0e5546aec6f3bfced08ce10c51f434e17f3c6210
ondanje <cyrilondanje@gmail.com>
2024-03-09 01:19:20 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    pascaltriangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = pascaltriangle[i - 1][j - 1] + pascaltriangle[i - 1][j]
+        pascaltriangle.append(row)
+
+    return pascaltriangle
