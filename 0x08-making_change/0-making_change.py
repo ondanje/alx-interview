7b940e8203fad347241347102a5ad9a31d47d339
ondanje <cyrilondanje@gmail.com>
2024-05-02 20:01:08 +0300

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """
+    def makeChange(coins, total)
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    """
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
